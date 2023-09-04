94e84c8146f8e6496ace91119a8d07d8c4386f05
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-03 23:22:37 -0400

    updated script - 03-09-2023

--- ssh-scp.sh
+++ ssh-scp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #welcome statement
-echo "Welcome to the ssh-scp assistant"
-echo "Select an option to continue"
-echo "1 - ssh"
-echo "2 - scp"
+printf "*** Welcome to the ssh-scp assistant ***\n\n"
+printf "Select an option to continue\n\n"
+echo "1 - Connect to a computer using ssh"
+echo "2 - Send/retrieve file using scp"
 
 read input
 
