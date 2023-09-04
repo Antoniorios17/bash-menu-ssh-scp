d4db4ac919ac3ad6caa998fb817ab20b9442b57a
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-03 23:08:14 -0400

    updated script"`date +"%d-%m-%Y"`"

--- ssh-scp.sh
+++ ssh-scp.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#welcome statement
+echo "Welcome to the ssh-scp assistant"
+echo "Select an option to continue"
+echo "1 - ssh"
+echo "2 - scp"
+
+read input
+
+case $input in
+    1) echo ""
+
+
+# provide the options of the program
