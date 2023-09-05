326698dc7e509a3a8b9b94ca90fb91cebbd3d29c
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-04 22:41:44 -0400

    updated script - 04-09-2023

--- ssh-scp.sh
+++ ssh-scp.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-#variables
+#absolute path to the keypair
 ssh_key=/home/antonio/awskey.pem
 
 #welcome statement
 printf "*** Welcome to the ssh-scp assistant ***\n\n"
-printf "Select an option to continue\n\n"
+printf "Select an option to continue:\n\n"
 echo "1. Connect to a computer using ssh"
 echo "2. Send/retrieve file using scp"
 
 
 read input
 
-# Function to execute the ssh option
-function ssh-assist {
-    echo "Welcome to the ssh assistant"
+# ip address- username
+function ip-username {
     echo "Please enter the Public IP address:"
     read ipaddress
     echo "Please enter the username:"
     read username
-    local remote_host="$username@$ipaddress"
+    declare -g $remote_host="$username@$ipaddress"
+}
+
+
+
+# Function to execute the ssh option
+function ssh-assist {
+    echo "Welcome to the ssh assistant"
+    ip-username
     echo "Connecting to $remote_host"
     sleep 1
     ssh -i $ssh_key $remote_host
@@ -29,6 +36,7 @@ function ssh-assist {
 
 function scp-assist {
     echo Hello I\'m the scp function
+    ssh-assist
 }
 
 
