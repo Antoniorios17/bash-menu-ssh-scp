0d5784ce6494ac7d5bbf176c6cb151ea8f27aac6
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-04 22:26:10 -0400

    updated script - 04-09-2023

--- ssh-scp.sh
+++ ssh-scp.sh
@@ -6,8 +6,8 @@ ssh_key=/home/antonio/awskey.pem
 #welcome statement
 printf "*** Welcome to the ssh-scp assistant ***\n\n"
 printf "Select an option to continue\n\n"
-echo "1 - Connect to a computer using ssh"
-echo "2 - Send/retrieve file using scp"
+echo "1. Connect to a computer using ssh"
+echo "2. Send/retrieve file using scp"
 
 
 read input
